9f8d4a274a7aac9a784c973347e462d69315c4f1
Brandon Hogan <me@brandonhogan.com>
2017-02-15 11:51:40 -0500

    Fixed an issue where the history tab would not update correctly to represent the differences done on the tracker tab

--- LiftScout/app/src/main/java/com/brandonhogan/liftscout/views/workout/history/HistoryPresenter.java
+++ LiftScout/app/src/main/java/com/brandonhogan/liftscout/views/workout/history/HistoryPresenter.java
@@ -80,4 +80,9 @@ public class HistoryPresenter implements HistoryContract.Presenter {
     public void viewCreated() {
         updateAdapter();
     }
+
+    @Override
+    public void update() {
+        updateAdapter();
+    }
 }
