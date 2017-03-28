623c7ca3a67c93c6cc83529e6e59d4eb6fa83f5c
Brandon Hogan <me@brandonhogan.com>
2017-03-28 18:33:41 -0400

    Basic category model unit tests

--- app/src/test/java/com/brandonhogan/liftscout/model/CategoryModelUnitTest.java
+++ app/src/test/java/com/brandonhogan/liftscout/model/CategoryModelUnitTest.java
@@ -1,7 +1,6 @@
 package com.brandonhogan.liftscout.model;
 
 import android.content.Context;
-
 import com.brandonhogan.liftscout.R;
 import com.brandonhogan.liftscout.repository.model.Category;
 
@@ -22,23 +21,49 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class CategoryModelUnitTest {
 
-    private static final String FAKE_STRING = "Lift Scout";
+    private static final String FAKE_STRING = "Fake Category Name";
+    private static final int FAKE_INT = 123456;
 
     @Mock
     Context mMockContext;
 
     @Test
-    public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
-        // Given a mocked Context injected into the object under test...
-        when(mMockContext.getString(R.string.app_name))
-                .thenReturn(FAKE_STRING);
+    public void CategoryModel_CorrectId_ReturnsTrue() {
         Category myCategory = new Category();
-        myCategory.setName("Lift Scout");
+        myCategory.setId(FAKE_INT);
+
+        // ...when the string is returned from the object under test...
+        int result = myCategory.getId();
+
+        // ...then the result should be the expected one.
+        assertThat(result, is(FAKE_INT));
+    }
+
+    @Test
+    public void CategoryModel_CorrectName_ReturnsTrue() {
+        // Given a mocked Context injected into the object under test...
+        when(mMockContext.getString(R.string.category_abs))
+                .thenReturn(FAKE_STRING);
+
+        Category myCategory = new Category();
+        myCategory.setName(mMockContext.getString(R.string.category_abs));
 
         // ...when the string is returned from the object under test...
         String result = myCategory.getName();
 
         // ...then the result should be the expected one.
         assertThat(result, is(FAKE_STRING));
+    }
+
+    @Test
+    public void CategoryModel_CorrectColor_ReturnsTrue() {
+        Category myCategory = new Category();
+        myCategory.setColor(FAKE_INT);
+
+        // ...when the string is returned from the object under test...
+        int result = myCategory.getColor();
+
+        // ...then the result should be the expected one.
+        assertThat(result, is(FAKE_INT));
     }
 }
