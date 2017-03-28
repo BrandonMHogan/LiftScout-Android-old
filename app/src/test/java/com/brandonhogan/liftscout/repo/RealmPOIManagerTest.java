ede604ff6c4c789045bf9acd3098ff155435374d
Brandon Hogan <me@brandonhogan.com>
2017-03-28 17:42:44 -0400

    created a realm unit test to verify I can mock realm models, and updated Realm to 3.0.0

---
+++ app/src/test/java/com/brandonhogan/liftscout/repo/RealmPOIManagerTest.java
@@ -0,0 +1,72 @@
+package com.brandonhogan.liftscout.repo;
+
+import com.brandonhogan.liftscout.BuildConfig;
+import com.brandonhogan.liftscout.repository.model.Exercise;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.core.classloader.annotations.SuppressStaticInitializationFor;
+import org.powermock.modules.junit4.rule.PowerMockRule;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import io.realm.Realm;
+import io.realm.log.RealmLog;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, manifest = Config.NONE, sdk = 19)
+@PowerMockIgnore({"org.mockito.*", "org.robolectric.*", "android.*"})
+@SuppressStaticInitializationFor("io.realm.internal.Util")
+@PrepareForTest({Realm.class, RealmLog.class})
+public class RealmPOIManagerTest {
+
+
+    @Rule
+    public PowerMockRule rule = new PowerMockRule();
+    Realm mockRealm;
+
+    @Before
+    public void setup() {
+        mockStatic(RealmLog.class);
+        mockStatic(Realm.class);
+
+        Realm mockRealm = PowerMockito.mock(Realm.class);
+        when(Realm.getDefaultInstance()).thenReturn(mockRealm);
+
+        this.mockRealm = mockRealm;
+    }
+
+    @Test
+    public void shouldBeAbleToGetDefaultInstance() {
+        assertThat(Realm.getDefaultInstance(), is(mockRealm));
+    }
+
+    @Test
+    public void shouldBeAbleToMockRealmMethods() {
+        when(mockRealm.isAutoRefresh()).thenReturn(true);
+        assertThat(mockRealm.isAutoRefresh(), is(true));
+
+        when(mockRealm.isAutoRefresh()).thenReturn(false);
+        assertThat(mockRealm.isAutoRefresh(), is(false));
+    }
+
+    @Test
+    public void shouldBeAbleToCreateARealmObject() {
+        Exercise exercise = new Exercise();
+        when(mockRealm.createObject(Exercise.class)).thenReturn(exercise);
+
+        Exercise output = mockRealm.createObject(Exercise.class);
+
+        assertThat(output, is(exercise));
+    }
+}
---
+++ app/src/test/java/rx/Observable.java
@@ -0,0 +1,9 @@
+package rx;
+
+/**
+ * Created by Brandon on 3/28/2017.
+ * Description :
+ */
+
+public class Observable {
+}
